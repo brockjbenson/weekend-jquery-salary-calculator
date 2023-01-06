121df3e1348c67ebf09e4356fca59acde71965a7
Brock Benson <brockjbenson@outlook.com>
2023-01-06 16:04:30 -0600

    more js work, adding the employees array and comments

--- jQuery Slaray Calc/script.js
+++ jQuery Slaray Calc/script.js
@@ -1,9 +1,13 @@
 $(function () {
+  // Making sure we are in js and jquery
   console.log("In jquery");
 
-  $("#submit").on("click", addEmp);
+  $("#submit").on("click", addEmp); // Adding our first event listener to be on the click of our submit button
 });
 
+let employees = []; // Create an array to store the employees
+
 function addEmp() {
+  // Function to add employees
   console.log("Adding Employee");
 }
