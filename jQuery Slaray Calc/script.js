797015db72f26f16851e0b3889de4fe2e9afc47d
Brock Benson <brockjbenson@outlook.com>
2023-01-06 16:38:29 -0600

    comments for the script

--- jQuery Slaray Calc/script.js
+++ jQuery Slaray Calc/script.js
@@ -8,23 +8,27 @@ $(function () {
 const employees = []; // Create an array to store the employees
 
 function addToEmployees(first, last, id, title, salary) {
+  // Creating a function to create a new employee and add to array
   const employee = {
-    firstName: first,
+    // create an employee object
+    firstName: first, // Add all of the values for the employee and set them to the coresponding argument
     lastName: last,
     idNumber: id,
     jobTitle: title,
     annualSalary: salary,
   };
-  employees.push(employee);
+  employees.push(employee); // Push the object into our array
 }
 
 function appendEmp() {
-  // Function to add employees
-  $("#table-body").empty();
+  // Create our appending function to append to the dom
+  $("#table-body").empty(); // Use this to clear the children of table-body
   for (let i = 0; i < employees.length; i++) {
-    const element = employees[i];
+    //loop over our array of employees
+    const element = employees[i]; // set a constant variable element to be employees at our index
 
-    $("#table-body").append(`
+    // create our append event by taking our element or employees at i and then the selector to target which value we want
+    $("#table-body").append(` 
       <tr>
       <td>${element.firstName}</td>
       <td>${element.lastName}</td>
@@ -37,19 +41,20 @@ function appendEmp() {
 }
 
 function handleSubmit() {
-  const firstVal = $("#first-name").val();
+  // Create our submit button function
+  const firstVal = $("#first-name").val(); // set a constant var to the values we want and set it equal to our input values from our hmtl
   const lastVal = $("#last-name").val();
   const idVal = $("#id-number").val();
   const titleVal = $("#job-title").val();
   const salaryVal = $("#annual-salary").val();
 
-  addToEmployees(firstVal, lastVal, idVal, titleVal, salaryVal);
+  addToEmployees(firstVal, lastVal, idVal, titleVal, salaryVal); // take those inputs from the html and use our addToEmployee function to add them to our employees array
 
-  $("#first-name").val("");
+  $("#first-name").val(""); // use this to clear our inputs after the submit button is clicked
   $("#last-name").val("");
   $("#id-number").val("");
   $("#job-title").val("");
   $("#annual-salary").val("");
 
-  appendEmp();
+  appendEmp(); // also run our append function when the submit function is clicked
 }
