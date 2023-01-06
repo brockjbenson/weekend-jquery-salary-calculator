4c0df1e98dc7bf171488af2d9120d9a2bf9cb5b1
Brock Benson <brockjbenson@outlook.com>
2023-01-06 16:31:22 -0600

    set up adding employee into the employees array as well as appending to dom

--- jQuery Slaray Calc/script.js
+++ jQuery Slaray Calc/script.js
@@ -2,12 +2,54 @@ $(function () {
   // Making sure we are in js and jquery
   console.log("In jquery");
 
-  $("#submit").on("click", addEmp); // Adding our first event listener to be on the click of our submit button
+  $("#submit").on("click", handleSubmit); // Adding our first event listener to be on the click of our submit button
 });
 
-let employees = []; // Create an array to store the employees
+const employees = []; // Create an array to store the employees
 
-function addEmp() {
+function addToEmployees(first, last, id, title, salary) {
+  const employee = {
+    firstName: first,
+    lastName: last,
+    idNumber: id,
+    jobTitle: title,
+    annualSalary: salary,
+  };
+  employees.push(employee);
+}
+
+function appendEmp() {
   // Function to add employees
-  console.log("Adding Employee");
+  $("#table-body").empty();
+  for (let i = 0; i < employees.length; i++) {
+    const element = employees[i];
+
+    $("#table-body").append(`
+      <tr>
+      <td>${element.firstName}</td>
+      <td>${element.lastName}</td>
+      <td>${element.idNumber}</td>
+      <td>${element.jobTitle}</td>
+      <td>${element.annualSalary}</td>
+      </tr>
+      `);
+  }
+}
+
+function handleSubmit() {
+  const firstVal = $("#first-name").val();
+  const lastVal = $("#last-name").val();
+  const idVal = $("#id-number").val();
+  const titleVal = $("#job-title").val();
+  const salaryVal = $("#annual-salary").val();
+
+  addToEmployees(firstVal, lastVal, idVal, titleVal, salaryVal);
+
+  $("#first-name").val("");
+  $("#last-name").val("");
+  $("#id-number").val("");
+  $("#job-title").val("");
+  $("#annual-salary").val("");
+
+  appendEmp();
 }
